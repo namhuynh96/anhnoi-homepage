02da24a944e2a97d8b2acc808822f62169dfee8b
Nam Huynh <huynhchinam96@gmail.com>
2021-01-05 12:26:37 +0200

    Add fronend

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join("public", "anhnoi-homepage", "build")));
+app.use(express.static(path.join("anhnoi-homepage-public", "build")));
 app.use((req, res, next) => {
   res.sendFile(
-    path.resolve(__dirname, "public", "anhnoi-homepage", "build", "index.html")
+    path.resolve(__dirname, "anhnoi-homepage-public", "build", "index.html")
   );
 });
 
